a44c5c8e3e96fe7fb46d1c56a549a3fd6ae36dc6
krzaku29 <krzaku29@gmail.com>
2014-04-24 22:18:25 +0200

    Add main class

---
+++ README.md
@@ -0,0 +1,4 @@
+Warms
+=====
+
+It's a little game made for semester project on my faculty. It is like real-time Schorched Earth. Will be written in Java. For more information see Warms - design.pdf (only Polish version avaliable sorry ;< ).
---
+++ src/pl/krzaku/proz/Game.java
@@ -0,0 +1,23 @@
+/**
+ * @author 		Patryk Majkrzak
+ * @version	1.0
+ */
+package pl.krzaku.proz;
+
+/**
+ * @author		Patryk Majkrzak
+ *
+ */
+public class Game
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
