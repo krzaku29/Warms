e7aa9223f3a0c1bc6756acb02f0ad1d5ef716d77
krzaku29 <majkrzak.patryk@gmail.com>
2014-05-29 23:19:47 +0200

    Just another commit message

--- src/pl/krzaku/proz/view/ContentManager.java
+++ src/pl/krzaku/proz/view/ContentManager.java
@@ -1,10 +1,34 @@
 package pl.krzaku.proz.view;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 import org.newdawn.slick.Music;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.Sound;
 
 public class ContentManager 
 {
+	private ArrayList<Music> musicList = new ArrayList<Music>();
+	private ArrayList<Sound> soundEffectList = new ArrayList<Sound>();
+	private ArrayList<SpriteSheet> spriteList = new ArrayList<SpriteSheet>();
+	
+	public ContentManager()
+	{
+		
+	}
+	
+	public void addMusic(String filename) 
+	{
+		try
+		{
+			musicList.add(new Music(filename));
+		} 
+		catch (SlickException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
 }
